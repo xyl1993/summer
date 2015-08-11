817ee0441a26b4aaa199ddf27dddc5b7cd8e02f9
unknown <602165057@qq.com>
2015-08-11 17:46:03 +0800

    Signed-off-by: unknown <602165057@qq.com>

--- public/my-javascript/home.js
+++ public/my-javascript/home.js
@@ -13,26 +13,21 @@ mainModule.controller('homeCtrl', ['$scope', function(){
 		if(count===4){count=0;}
 	},5000); /*自动切换时间(毫秒)*/
 
-	
+
 }]);
 
-// mainModule.directive('changeimg', function(){
-// 	// Runs during compile
-// 	return {
-// 		link: function($scope, iElm, iAttrs) {
-			
-// 			$(iElm).hover(function() {
-// 				var lId = iElm[0].id;
-// 				if(lId==='about'){
-// 					$('.header').css('background-image', 'url(../images/page2.jpg)');
-// 				}else if(lId==='home'){
-// 					$('.header').css('background-image', 'url(../images/page1.jpg)');
-// 				}else if(lId==='experience'){
-// 					$('.header').css('background-image', 'url(../images/page3.jpg)');
-// 				}else if(lId==='skills'){
-// 					$('.header').css('background-image', 'url(../images/page4.jpg)');
-// 				}
-// 			});
-// 		}
-// 	};
-// });
+mainModule.directive('imgHover', function(){
+	// Runs during compile
+	return {
+		link: function($scope, iElm, iAttrs) {
+			$(iElm).hover(function() {
+				var lClass = iElm[0].className;
+				if(lClass === "meself"){
+					 $(".meself-hover").slideToggle("slow");
+				}else if(lClass === "experience"){
+					 $(".experience-hover").slideToggle("slow");
+				}
+			});
+		}
+	};
+});
